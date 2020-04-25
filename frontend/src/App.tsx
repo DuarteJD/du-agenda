1dd8cbd3102fa087bb948e7bc346b6e7925fad3d
Julio Duarte <jcduarte.ti@gmail.com>
2020-04-25 16:28:26 -0300

    Create Toast component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 // import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
@@ -9,6 +10,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
