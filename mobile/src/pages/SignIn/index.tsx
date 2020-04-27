68cbe7794b6c7d02cfaabaab251cd6b09f3ad0fe
Julio Duarte <jcduarte.ti@gmail.com>
2020-04-26 22:39:35 -0300

    SignIn screen

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,30 +1,70 @@
 import React from 'react';
-import { StatusBar, Image } from 'react-native';
+import {
+  StatusBar,
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <Container>
-        <Image source={logoImg} />
-        <Title>Faça seu logon</Title>
-
-        <Input name="email" icon="mail" placeholder="E-mail" />
-        <Input name="password" placeholder="Senha" />
-        <Button
-          onPress={() => {
-            console.log('teste');
-          }}
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
         >
-          Entrar
-        </Button>
-      </Container>
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log('teste');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword
+              onPress={() => {
+                console.log('');
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton>
+        <FeatherIcon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+      </CreateAccountButton>
     </>
   );
 };
