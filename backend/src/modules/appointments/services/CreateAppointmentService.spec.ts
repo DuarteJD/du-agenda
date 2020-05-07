79abed2bb7a0c6cb37784a0602b7ce342eadf2ec
Julio Duarte <jcduarte.ti@gmail.com>
2020-05-06 23:48:19 -0300

    Add jest

---
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,11 @@
+// import CreateAppointmentService from './CreateAppointmentService';
+
+// describe('CreateAppointment', () => {
+//   it('should be able to create a new appointment', () => {
+//     expect();
+//   });
+
+//   // it('should not be able to create two appointment on the same datetime', () => {
+//   //   expect();
+//   // });
+// });
