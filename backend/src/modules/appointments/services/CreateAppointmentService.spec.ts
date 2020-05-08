f92c6445dedb34bdf3a546a0868f220083e7cfbb
Julio Duarte <jcduarte.ti@gmail.com>
2020-05-07 21:02:32 -0300

    first tdd appointment

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,11 +1,24 @@
-// import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
 
-// describe('CreateAppointment', () => {
-//   it('should be able to create a new appointment', () => {
-//     expect();
-//   });
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
 
-//   // it('should not be able to create two appointment on the same datetime', () => {
-//   //   expect();
-//   // });
-// });
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456');
+  });
+
+  // it('should not be able to create two appointment on the same datetime', () => {
+  //   expect();
+  // });
+});
