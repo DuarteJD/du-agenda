e596eca99e9558abf251c8048ad78ab6fe9b3f72
Julio Duarte <jcduarte.ti@gmail.com>
2020-05-08 23:38:09 -0300

    Routes plus controllers reset password

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import appointmentRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import userRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 import authentication from '@modules/users/infra/http/middlewares/authentication';
 
@@ -10,6 +11,7 @@ const routes = Router();
 
 routes.use('/sessions', sessionsRouter);
 routes.use('/users', userRouter);
+routes.use('/password', passwordRouter);
 routes.use(authentication);
 routes.use('/appointments', appointmentRouter);
 
